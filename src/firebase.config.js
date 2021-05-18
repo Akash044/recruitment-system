9995e17d314928faebdc122f937d9e94e5876ebb
Md. Akash Hossain <akash.cse.ruet@gmail.com>
2021-05-19 05:56:49 +0600

    first commit

---
+++ src/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCyz5zPUfvqmWxVcpKzbtqTQkr3qPdXfd8",
+    authDomain: "recruitment-system-69f17.firebaseapp.com",
+    projectId: "recruitment-system-69f17",
+    storageBucket: "recruitment-system-69f17.appspot.com",
+    messagingSenderId: "499211804940",
+    appId: "1:499211804940:web:d73916b4cc7ffe02402929"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  export default firebaseConfig;
